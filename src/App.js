6ab0a2f9ec2027c0feaae3b068e55833111e7917
Mukeshwar S <mukeshwar.s@Incture.local>
2023-04-21 16:53:53 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { AiFillGithub } from "react-icons/ai";
+
+function App() {
+  return (
+    <div className="App">
+      <AiFillGithub/>
+      <span> Home</span>
+    </div>
+  );
+}
+
+export default App;
